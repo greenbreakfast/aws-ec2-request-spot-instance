17ae2230b965895be10c8902391b41f322436eb1
Lazar <lazardemin@gmail.com>
2021-04-14 10:11:53 -0400

    now writes launched instance info to a file

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import time
+from datetime import datetime
 
 def systemCall(inputCmd):
     # create array from command
@@ -26,30 +27,62 @@ def systemCallJson(inputCmd):
         raise SystemExit(e)
     return ret
 
+def printInstanceInfo(info):
+    print(' ')
+    print(' ')
+    print(' ')
+
+    print('*'*20)
+    print('Spot instance launched and running')
+    print('\tInstance ID:\t\t%s'%info['instanceId'])
+    print('\tPublic IP Address:\t%s'%info['ipAddr'])
+    print('\tSpot instance price:\t$%s / hr'%info['instancePrice'])
+    print(' ')
+    print('To shut down instance, run:')
+    print('\taws ec2 cancel-spot-instance-requests --spot-instance-request-ids %s'%info['spotRequestId'])
+    print('\taws ec2 terminate-instances --instance-ids %s'%info['instanceId'])
+    print('*'*20)
+
+def printInstanceInfoToFile(info, fileName):
+    with open(fileName, "w") as f:
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        f.write('Spot instance launched and running at %s\n'%dt_string)
+        f.write('\tInstance ID:\t\t%s\n'%info['instanceId'])
+        f.write('\tPublic IP Address:\t%s\n'%info['ipAddr'])
+        f.write('\tSpot instance price:\t$%s / hr\n'%info['instancePrice'])
+        f.write(' \n')
+        f.write('To shut down instance, run:\n')
+        f.write('\taws ec2 cancel-spot-instance-requests --spot-instance-request-ids %s\n'%info['spotRequestId'])
+        f.write('\taws ec2 terminate-instances --instance-ids %s\n'%info['instanceId'])
+
+
 def main():
     print(' Creating spot instance request ...')
+    info = {}
     cmd = 'aws ec2 request-spot-instances --instance-count 1 --type one-time --launch-specification file://specification.json'
     ret = systemCallJson(cmd)
-    spotRequestId = ret['SpotInstanceRequests'][0]['SpotInstanceRequestId']
+    info['spotRequestId'] = ret['SpotInstanceRequests'][0]['SpotInstanceRequestId']
 
-    print(' Spot instance request created, id is %s'%spotRequestId)
+    print(' Spot instance request created, id is %s'%info['spotRequestId'])
 
 
     print(' Waiting until spot instance is fulfilled ...')
-    instanceId = ''
-    instancePrice = ''
+    info['instanceId'] = ''
+    info['instancePrice'] = ''
     count = 0
 
     while True:
         time.sleep(5)
-        cmd = 'aws ec2 describe-spot-instance-requests --spot-instance-request-ids %s'%spotRequestId
+        cmd = 'aws ec2 describe-spot-instance-requests --spot-instance-request-ids %s'%info['spotRequestId']
         ret = systemCallJson(cmd)
         # print(ret['SpotInstanceRequests'][0]['Status'])
         status = ret['SpotInstanceRequests'][0]['Status']['Code']
 
         if status == 'fulfilled':
-            instanceId = ret['SpotInstanceRequests'][0]['InstanceId']
-            instancePrice = ret['SpotInstanceRequests'][0]['SpotPrice']
+            info['instanceId'] = ret['SpotInstanceRequests'][0]['InstanceId']
+            info['instancePrice'] = ret['SpotInstanceRequests'][0]['SpotPrice']
             break
         print(' .', end = '')
         count += 1
@@ -57,38 +90,26 @@ def main():
             print('\n   - Spot Instance Request status: %s'%status)
 
     print(' Waiting for instance to initialize ...')
-    ipAddr = ''
+    info['ipAddr'] = ''
     count = 0
 
     while True:
         time.sleep(5)
-        cmd = 'aws ec2 describe-instances --instance-ids %s'%instanceId
+        cmd = 'aws ec2 describe-instances --instance-ids %s'%info['instanceId']
         ret = systemCallJson(cmd)
 
         status = ret['Reservations'][0]['Instances'][0]['State']['Name']
 
         if status == 'running':
-            ipAddr = ret['Reservations'][0]['Instances'][0]['PublicIpAddress']
+            info['ipAddr'] = ret['Reservations'][0]['Instances'][0]['PublicIpAddress']
             break
         print(' .', end = '')
         count += 1
         if count == 5:
             print('\n   - Instance status: %s'%status)
 
-    print(' ')
-    print(' ')
-    print(' ')
-
-    print('*'*20)
-    print('Spot instance launched and running')
-    print('\tInstance ID:\t\t%s'%instanceId)
-    print('\tPublic IP Address:\t%s'%ipAddr)
-    print('\tSpot instance price:\t$%s / hr'%instancePrice)
-    print(' ')
-    print('To shut down instance, run:')
-    print('\taws ec2 cancel-spot-instance-requests --spot-instance-request-ids %s'%spotRequestId)
-    print('\taws ec2 terminate-instances --instance-ids %s'%instanceId)
-    print('*'*20)
+    printInstanceInfo(info)
+    printInstanceInfoToFile(info, './instance.txt')
 
 if __name__ == '__main__':
     main()
